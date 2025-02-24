c4a1e4ee9fc655e4bb38168b987d165933ffaf26
Antares Valle <antares.valle@gmail.com>
2025-02-24 14:41:14 -0800

    create ProductCard and AddToCart components

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const AddToCart = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('Click Add to cart')}>Add to cart</button>
+    </div>
+  )
+}
+
+export default AddToCart
