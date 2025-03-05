5ca2d7dfb628d31126e9a7072bf6ce6ac2c7cfe3
Antares Valle <antares.valle@gmail.com>
2025-03-05 11:14:57 -0800

    handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,11 +2,20 @@
 import { TextField, Button, Spinner } from "@radix-ui/themes";
 import { useState, useEffect } from 'react'
 import dynamic from "next/dynamic";
+import { useForm, Controller } from 'react-hook-form';
+import { useRouter } from "next/navigation";
+import axios from 'axios';
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false });
 import "easymde/dist/easymde.min.css";
 
+interface IssueForm {
+    title: string;
+    description: string;
+}
+
 const NewIssuePage = () => {
-    
+    const router = useRouter();
+    const { register, control, handleSubmit } = useForm<IssueForm>();
     // Temporary workaround for hydration issue with RadixUI Text components. 
     // TODO: Update once it's fixed.
     const [isClient, setIsClient] = useState(false);
@@ -16,15 +25,24 @@ const NewIssuePage = () => {
     }, []);
 
   return (
-    <div className="max-w-lg space-y-3">
+    <form className="max-w-lg space-y-3" 
+        onSubmit={ handleSubmit(async (data) => {
+            await axios.post('/api/issues', data)
+            router.push('/issues');
+        })}
+    >
         { isClient ? 
             <>
-                <TextField.Root placeholder="Title" />
-                <SimpleMDE placeholder="Description"/>
+                <TextField.Root placeholder="Title" {...register('title')} />
+                <Controller
+                    name="description"
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder="Description"{...field}/>}
+                />
                 <Button>Submit New Issue</Button> 
             </>
         : <Spinner />}
-    </div>
+    </form>
   )
 }
 
